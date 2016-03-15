1d72ede4c39a0385605bfe4d96113ebcc581c876
Balázs Buri <info@burib.net>
2016-03-15 15:04:01 +0100

    feat(ghost) trying to make express working.

--- usr/template/app.js
+++ usr/template/app.js
@@ -1,51 +1,30 @@
-var http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env,
-      ghost = require('ghost');
+var http = require('http'),
+  fs = require('fs'),
+  path = require('path'),
+  sysInfo = require('./utils/sys-info'),
+  env = process.env,
+  ghost = require('ghost'),
+  express = require('express'),
+  app = express();
 
 ghost({
   config: path.join(__dirname, '/ghost_settings/config.js')
-}).then(function (ghostServer) {
-  ghostServer.start();
-});
+}).then(function(ghostServer) {
+  app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+  ghostServer.start(app);
 
-//
-//let server = http.createServer(function (req, res) {
-//  let url = req.url;
-//  if (url == '/') {
-//    url += 'index.html';
-//  }
-//
-//  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-//  //            for OpenShift health monitoring
-//
-//  if (url == '/health') {
-//    res.writeHead(200);
-//    res.end();
-//  } else if (url.indexOf('/info/') == 0) {
-//    res.setHeader('Content-Type', 'application/json');
-//    res.setHeader('Cache-Control', 'no-cache, no-store');
-//    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-//  } else {
-//    fs.readFile('./static' + url, function (err, data) {
-//      if (err) {
-//        res.writeHead(404);
-//        res.end();
-//      } else {
-//        let ext = path.extname(url).slice(1);
-//        res.setHeader('Content-Type', contentTypes[ext]);
-//        if (ext === 'html') {
-//          res.setHeader('Cache-Control', 'no-cache, no-store');
-//        }
-//        res.end(data);
-//      }
-//    });
-//  }
-//});
-//
-//server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-//  console.log(`Application worker ${process.pid} started...`);
-//});
+  app.get('/', function(req, res) {
+    res.send('Hello node express app..');
+  });
+
+  app.get('/health', function(req, res) {
+    res.writeHead(200);
+    res.end();
+  });
+
+  app.get('/stats', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 'no-cache, no-store');
+    res.end(JSON.stringify(sysInfo.getStats()));
+  });
+});
